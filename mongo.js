273d4ab5234aecc56bb1186e440591b31c8cdc34
Alisa <alisa.luoma@hotmail.fi>
2023-06-13 13:08:54 +0300

    Task 3.12

---
+++ mongo.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+
+if(process.argv.length < 3) {
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =  `mongodb+srv://opiskeliAlli:${password}@cluster0.3y38r9f.mongodb.net/phonecontactsApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const personShema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model('Person', personShema)
+
+if(process.argv.length < 4) {
+    console.log('phonebook:')
+    Person.find({}).then(persons =>{
+        persons.forEach(person => {
+            console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+        process.exit(1)
+    })
+} else if (process.argv.length == 5) {
+    const name = process.argv[3]
+    const number = process.argv[4]
+    
+    const person = new Person({
+        name: name,
+        number: number
+    })
+    
+    person.save().then(result => {
+        console.log(`Added ${name} number ${number} to phonebook`)
+        mongoose.connection.close()
+    })
+} else {
+    console.log('Need more data')
+}
